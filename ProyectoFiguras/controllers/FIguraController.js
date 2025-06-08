13210cec0393ced95ca5c603a9166e6fc0bd2161
LuisE23 <luise10.cb@gmail.com>
2025-06-07 21:28:26 -0500

    ultimos arreglos, aun faltan correciones

--- ProyectoFiguras/controllers/FIguraController.js
+++ ProyectoFiguras/controllers/FIguraController.js
@@ -9,10 +9,12 @@ class FiguraController{
         this.figuraServices = new FiguraService();
         this.formularioCalcularArea=this.formularioCalcularArea.bind(this);
         this.calcularAreaFiguras=this.calcularAreaFiguras.bind(this);
+        this.calcularPerimetroFiguras=this.calcularPerimetroFiguras(this);
     }
 
     formularioCalcularArea(request, response){
         response.render ('area');
+        response.render ('perimetro');
     }
 
     calcularAreaFiguras(request, response) {
@@ -29,6 +31,20 @@ class FiguraController{
         }
         response.render ('area', {resultado});
     }
+    calcularPerimetroFiguras(request, response) {
+        let {figura, baseMayor, baseMenor, base, lado, lado1, lado2} = request.body;
+        let resultado;
+        if(figura=== 'rombo'){
+            resultado = this.figuraServices.calcularPerimetroRombo(lado);
+        } 
+        else if (figura === 'trapecio'){           
+            resultado = this.figuraServices.calcularPerimetroTrapecio(Number(baseMayor), Number(baseMenor), Number(lado1), Number(lado2));        
+        } 
+        else if (figura === 'paralelogramo'){           
+            resultado = this.figuraServices.calcularPerimetroParalelogramo(base, lado);
+        }
+        response.render ('perimetro', {resultado});
     
+    }
 }
 export default FiguraController;
